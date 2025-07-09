f448438cef9300d58b1f53f2b7e0c19b33c4b638
sachin-swain <sachin310105@gmail.com>
2025-07-10 02:43:55 +0530

    hii

--- passWordGenerator/vite.config.js
+++ passWordGenerator/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Password-Generator/",
+  base: '/Password-Generator/',
   plugins: [react() , tailwindcss()],
 
 })
