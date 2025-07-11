21183906cbc34a1f360ff389fe7f8d8652c66482
sachin-swain <sachin310105@gmail.com>
2025-07-11 14:03:13 +0530

    deteted files

--- password/vite.config.js
+++ password/vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/password/',
-  plugins: [react(), tailwindcss()],
+  plugins: [react() , tailwindcss()],
 })
